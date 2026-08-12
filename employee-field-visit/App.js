0689028a75f6b4d939757216e72a59771127b97f
jaintulya <tulya.jain.cg@gmail.com>
2026-08-12 16:37:31 +0530

    refactor(field-visit): remove legacy App and screen components

--- employee-field-visit/App.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-import LoginScreen from './src/screens/LoginScreen';
-import HomeScreen from './src/screens/HomeScreen';
-
-export default function App() {
-  const [employeeName, setEmployeeName] = useState(null);
-  return employeeName ? <HomeScreen employeeName={employeeName} onLogout={() => setEmployeeName(null)} /> : <LoginScreen onLogin={setEmployeeName} />;
-}
